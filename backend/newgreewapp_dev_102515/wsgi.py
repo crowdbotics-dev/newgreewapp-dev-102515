de86006146318aa493df812d71f00717efc4df39
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 07:10:24 +0000

    Initial commit.

---
+++ backend/newgreewapp_dev_102515/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for newgreewapp_dev_102515 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newgreewapp_dev_102515.settings')
+
+application = get_wsgi_application()
